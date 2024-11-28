1681034416526078afe9f777e819535c61a65cef
Stepan Trubcov <stepantrubcov@gmail.com>
2024-11-28 17:23:31 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
 import logo from './logo.svg';
-import './App.css';
+import c from './App.module.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={c.home}>
+      <div className={c.text} >Хор Ржевской Покровской Старообрядческой общины</div>
+      <div className={c.div} >
+      <div className={c.buttons} >
+      <a href="/" className={c.a} > <img className={c.img} src='https://cdn.icon-icons.com/icons2/2429/PNG/512/telegram_logo_icon_147228.png' />Телеграм </a>
+      <a href="/" className={c.a}> <img className={c.img} src='https://img.icons8.com/?size=80&id=FHhIGGevoHnP&format=png' />Яндекс Музыка</a>
+      <a href="/" className={c.a}> <img className={c.img} src='https://cdn.icon-icons.com/icons2/836/PNG/512/Youtube_icon-icons.com_66802.png' />YouTube</a>
+      <a href="/" className={c.a}> <img className={c.img} src='https://www.ph4.org/_RU/DL/LOGO_ICON/r/rutube.gif' />Rutube</a>
+      </div>
+      </div>
     </div>
   );
 }
