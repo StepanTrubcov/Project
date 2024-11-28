c97475c4c99bea1e049fd28021c01fd337ffd147
Stepan Trubcov <stepantrubcov@gmail.com>
2024-11-28 18:02:26 +0300

    Изменения стиля

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import c from './App.module.css';
+import music from './img/music.svg'
+import telegram from './img/telegram.svg'
+import YouTube from './img/YouTube.svg'
 
 const App = () => {
   return (
@@ -7,10 +10,10 @@ const App = () => {
       <div className={c.text} >Хор Ржевской Покровской Старообрядческой общины</div>
       <div className={c.div} >
       <div className={c.buttons} >
-      <a href="/" className={c.a} > <img className={c.img} src='https://cdn.icon-icons.com/icons2/2429/PNG/512/telegram_logo_icon_147228.png' />Телеграм </a>
-      <a href="/" className={c.a}> <img className={c.img} src='https://img.icons8.com/?size=80&id=FHhIGGevoHnP&format=png' />Яндекс Музыка</a>
-      <a href="/" className={c.a}> <img className={c.img} src='https://cdn.icon-icons.com/icons2/836/PNG/512/Youtube_icon-icons.com_66802.png' />YouTube</a>
-      <a href="/" className={c.a}> <img className={c.img} src='https://www.ph4.org/_RU/DL/LOGO_ICON/r/rutube.gif' />Rutube</a>
+      <a href="/" className={c.a} > <img className={c.img} src={telegram} /> <div className={c.name} >Телеграм</div></a>
+      <a href="/" className={c.a}> <img className={c.img} src={music} /><div className={c.name} >Яндекс Музыка</div></a>
+      <a href="/" className={c.a}> <img className={c.img} src={YouTube} /><div className={c.name} >YouTube</div></a>
+      <a href="/" className={c.a}> <img className={c.img} src='https://www.svgrepo.com/show/504824/rutube.svg' /><div className={c.name} >Rutube</div></a>
       </div>
       </div>
     </div>
